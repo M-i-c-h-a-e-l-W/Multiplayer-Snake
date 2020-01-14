72215195d21e13aefdf5d98672a4b4cd0185adc0
Michael <michael.weishaar@gmx.de>
2020-01-14 15:34:36 +0100

    style

--- src/main/java/org/snake/configer/WebSocketConfigurator.java
+++ src/main/java/org/snake/configer/WebSocketConfigurator.java
@@ -1,15 +1,16 @@
 package org.snake.configer;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfigurator implements  WebSocketMessageBrokerConfigurer  {
+public class WebSocketConfigurator implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/comment/new","/comment/deleteById",
+        config.enableSimpleBroker("/comment/new", "/comment/deleteById",
                 "/snake/changeDofP", "/snake/fodderOfSnake", "/snake/deleted", "/snake/chat", "/snake/newHighScore");
         //config.enableSimpleBroker("/comment/deleteById");
     }
