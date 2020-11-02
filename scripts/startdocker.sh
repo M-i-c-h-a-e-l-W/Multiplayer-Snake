92f0aa06766df8fe4561f38cbd7c0241bebdc85a
Michael Weishaar <m.weishaar@micromata.de>
2020-11-02 12:07:12 +0100

    changed docker container name

--- scripts/startdocker.sh
+++ scripts/startdocker.sh
@@ -1,3 +1,3 @@
-docker stop some-postgres
-docker rm some-postgres
-docker run --name some-postgres -e POSTGRES_PASSWORD=test -e POSTGRES_USER=test -e POSTGRES_DB=test -p 127.0.0.1:5433:5432 -d postgres
+docker stop simple-postgresql
+docker rm simple-postgresql
+docker run --name simple-postgresql -e POSTGRES_PASSWORD=test -e POSTGRES_USER=test -e POSTGRES_DB=test -p 127.0.0.1:5433:5432 -d postgres
